afdd17f24831f1508911f2f51a545777dd18e2f1
Michael Zeitler <m.zeitler1987@gmail.com>
2024-06-29 23:18:23 +0200

    db.js added

--- scripts/script.js
+++ scripts/script.js
@@ -18,29 +18,6 @@ function init() {
     renderBasket(); 
 }
 
-let food = [
-    {
-        "name": "Gyrosteller mit Pommes",
-        "price": 11.50
-    },
-    {
-        "name": "Gemischte Grillplatte",
-        "price": 17.50
-    },
-    {
-        "name": "Pita mit Zaziki",
-        "price": 8.50
-    },
-    {
-        "name": "Meeresfrüchte Salat",
-        "price": 12.50
-    },
-    {
-        "name": "Hausgemachtes Dessert",
-        "price": 8.90
-    }
-];
-
 function renderFood() {
     let content = document.getElementById('foodContent');
     clearInnerHTML('foodContent');
