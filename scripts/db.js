fc09809819cf7b1643aec9271577c23d0d0f1837
Michael Zeitler <m.zeitler1987@gmail.com>
2024-06-30 19:08:38 +0200

    additional food data, edited category-nav

--- scripts/db.js
+++ scripts/db.js
@@ -1,11 +1,11 @@
 let food = [
     {
         "category": "Beliebt",
-        "dishes": ["Gyrosteller mit Pommes", "Gemischte Grillplatte", "Pita mit Zaziki", "Meeresfrüchte Salat", "Tiramisu"],
+        "dishes": ["Gyrosteller mit Pommes", "Gemischte Grillplatte", "Pita mit Tzatziki", "Meeresfrüchte Salat", "Tiramisu"],
         "descriptions": [
             "butterweiches Fleisch, serviert mit knusprigen Pommes Frittes",
             "das beste von Rind, Lamm und Huhn",
-            "wahlweise Weizen, Vollkorn oder Dinkel Pita mit würzigem Zaziki",
+            "wahlweise Weizen, Vollkorn oder Dinkel Pita mit würzigem Tzatziki",
             "erlesene Schätze des Mittelmeers auf einer Platte",
             "mit griechischem Joghurt, feinem Puderzucker und Mastix-Likör"
         ],
@@ -30,31 +30,81 @@ let food = [
             "erlesene Schätze des Mittelmeers auf einer Platte"
         ],
         "prices": [14.50, 12.90, 13.70]
+    },
+    {
+        "category": "Fleisch",
+        "dishes": ["Gyrosteller mit Pommes", "Pita mit Tzatziki", "Gemischte Grillplatte", "Lammhaxe", "Argentisches Rinderfilet"],
+        "descriptions": [
+            "Gyros vom Schwein mit Rosmarinkartoffeln und Tzatziki",
+            "wahlweise Weizen, Vollkorn oder Dinkel Pita mit würzigem Tzatziki",
+            "das beste von Rind, Lamm und Huhn",
+            "in Rotwein-Thymiansauce serviert mit Kartoffelpüree",
+            "200g zartes Rinderfilet mit Ofenkartoffeln und Rotweinsauce"
+        ],
+        "prices": [11.50, 8.50, 17.50, 28.50, 33.50]
+    },
+    {
+        "category": "Fisch",
+        "dishes": ["Doradenfilet", "Oktopus", "Scampis", "Fischplatte"],
+        "descriptions": [
+            "gegrillt mit Lauchgemüse, geschmorten Cocktailtomaten & Zitronen-Kräuter-Sauce",
+            "gegrillt auf rote Beete Carpaccio",
+            "gegrillt mit Rosmarinkartoffeln und Gemüse",
+            "mit Lachsfilet, Doradenfilet, Baby Calamares, Scampi & Gavros"
+        ],
+        "prices": [25.50, 27.50, 29.80, 23.20]
+    },
+    {
+        "category": "Vegetarisch",
+        "dishes": ["Gemista", "Vegetarische Moussaka", "Zucchini Schiffchen", "Gnocchi"],
+        "descriptions": [
+            "gefüllte Tomaten & Paprika mit Reis und Kräutern, serviert mit Tomatensauce",
+            "mit Auberginen, Kartoffeln, Karotten und Bechamelsauce",
+            "gefüllt mit Tomaten-Paprika-Gemüse und Fetakäse überbacken",
+            "gefüllt mit Oliven in Sahnesauce und geriebenem Fetakäse"
+        ],
+        "prices": [16.50, 15.30, 17.50, 12.70]
+    },
+    {
+        "category": "Dessert",
+        "dishes": ["Limetten Cheesecake","Tiramisu", "Griechischer Joghurt", "Galaktobureko", "Gemischte Dessert Platte"],
+        "descriptions": [
+            "mit Beerensauce",
+            "mit griechischem Joghurt, feinem Puderzucker und Mastix-Likör",
+            "mit Honig und Walnüssen",
+            "Blätterteig mit Vanille-Grießpudding-Füllung",
+            "etwas Süßes zum Abgang für 2 Personen"
+        ],
+        "prices": [6.50 ,8.90, 5.20, 5.60, 21.50]
+    },
+    {
+        "category": "Biere",
+        "dishes": ["Becks Pils","Corona Extra", "Spaten hell"],
+        "descriptions": [
+            "0.33 l",
+            "0.33 l",
+            "0.33 l"
+        ],
+        "prices": [5.50, 4.50, 5.30]
+    },
+    {
+        "category": "Wein",
+        "dishes": ["Primitivo - Tagaro Cinquenoci","Syrah-Merlot - Dyo Elies", "Ornellaia Rosso"],
+        "descriptions": [
+            "Flasche 0.75 l",
+            "Flasche 0.75 l",
+            "Flasche 0.75 l"
+        ],
+        "prices": [48.00, 61.00, 340.00]
+    },
+    {
+        "category": "Alkoholfrei",
+        "dishes": ["Selters classic","Saft","Red Bull"],
+        "descriptions": [
+            "natur - ohne Kohlensäure",
+            "nach Wahl: Apfel, Orange, Maracuja, Cranberry",
+            "Zero - ohne Zucker"
+        ],
+        "prices": [5.50, 4.30, 5.50]
     }
 ];
-
-
-
-
-// let food = [
-//     {
-//         "name": "Gyrosteller mit Pommes",
-//         "price": 11.50
-//     },
-//     {
-//         "name": "Gemischte Grillplatte",
-//         "price": 17.50
-//     },
-//     {
-//         "name": "Pita mit Zaziki",
-//         "price": 8.50
-//     },
-//     {
-//         "name": "Meeresfrüchte Salat",
-//         "price": 12.50
-//     },
-//     {
-//         "name": "Hausgemachtes Dessert",
-//         "price": 8.90
-//     }
-// ];
